f7a21ecdabfa85230fafa3f602191a4af2ca906a
Jorge H Baptista <jorgehenrique@live.com>
2021-02-11 09:22:03 -0300

    Commit init

---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,31 @@
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+  product: Product = {
+    name: '',
+    price: 0
+  }
+  
+  constructor(private productService: ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+    
+  }
+  createProduct(){
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Testando console");
+      this.router.navigate(['products']);
+    });
+  }
+  cancel(){
+    this.router.navigate(['products'])
+  }
+}
